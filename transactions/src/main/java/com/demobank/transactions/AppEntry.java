00257221ed42f06297b8b4cb479eec3d5d65ac29
Sib Mangena <sibmangena@bluffhillsolutions.com>
2017-12-07 02:59:10 -0500

    Setup demo api

---
+++ transactions/src/main/java/com/demobank/transactions/AppEntry.java
@@ -0,0 +1,11 @@
+package com.demobank.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEntry {
+    public static void main(final String[] args) {
+        SpringApplication.run(AppEntry.class, args);
+    }
+}
